ff258efa37c7cc78a4475fb5a4b42d9d610dec69
Felhasznalo <felhasznalo@MacBook-Pro-uzivatela-Felhasznalo.local>
2023-05-04 08:33:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+//Import Style
+import "./styles/app.scss";
+//Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav"
+//import util
+import data from "./data";
+import { faL } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 function App() {
+  //Ref
+    const audioRef = useRef(null);
+  //State
+const [songs, setSongs] = useState(data());
+const [currentSong, setCurrentSong] = useState(songs[0]);
+const [isPlaying, setIsPlaying] = useState(false);
+const [songInfo, setSongInfo] = useState({
+  currentTime: 0,
+  duration: 0,
+  animationPercentage: 0,
+});
+const [libraryStatus, setLibraryStatus] = useState(false);
+const timeUpdateHandler = (e) => {
+  const current = e.target.currentTime;
+  const duration = e.target.duration;
+  //calculate Percentage 
+  const roundedCurrent = Math.round(current);
+  const roundedDuration = Math.round(duration);
+  const animation = Math.round((roundedCurrent / roundedDuration ) * 100);
+  console.log(animation);   
+  setSongInfo({
+    ...songInfo, 
+    currentTime: current, 
+    duration,
+    animationPercentage: animation,
+  });
+};
+const songEndHandler = async () => {
+  //when the song ends skips forward
+  let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+  await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+
+  if (isPlaying) {
+    setTimeout(() => {
+      audioRef.current.play();
+    }, 100);
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong}/>
+      <Player 
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
+        />
+      <Library 
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs} 
+        libraryStatus={libraryStatus}
+        />
+      <audio 
+          onTimeUpdate={timeUpdateHandler} 
+          onLoadedMetadata={timeUpdateHandler}
+          ref={audioRef} 
+          src={currentSong.audio}
+          onEnded={songEndHandler}
+      ></audio>
     </div>
   );
 }
 
 export default App;
+  
